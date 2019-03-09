b3579cf69634c6b72eb3ad524568d33e429a6961
Felix Zongo <felix251@yahoo.fr>
2019-03-09 06:25:28 -0500

    CORECTION

--- app/src/main/java/com/example/week4test/MainActivity.java
+++ app/src/main/java/com/example/week4test/MainActivity.java
@@ -2,9 +2,9 @@ package com.example.week4test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-    static int[] control = {32,65,69,73,79,85,89,91,92,93,94,95,96,97,101,105,111,117,121};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +30,64 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private String encodedString(String toEncode) {
-        return toEncode;
+
+        char[] in  = toEncode.toCharArray();
+        int [] asciiIn = new int[in.length];
+        for (int i = 0; i < in.length; i++) {
+            asciiIn[i] = (int)in[i];
+        }
+        asciiIn = reversNum(asciiIn);
+
+        for (int i = 0; i < toEncode.length(); i++) {
+
+            if  (asciiIn[i] == 65 || asciiIn[i] == 97)      asciiIn[i] = 49;
+            else if(asciiIn[i] == 69 || asciiIn[i] == 101)  asciiIn[i] = 50;
+            else if (asciiIn[i] == 73 || asciiIn[i] == 105) asciiIn[i] = 51;
+            else if (asciiIn[i] == 79 || asciiIn[i] == 111) asciiIn[i] = 52;
+            else if (asciiIn[i] == 85 || asciiIn[i] == 117) asciiIn[i] = 23;
+            else if (asciiIn[i] == 89 || asciiIn[i] == 121) asciiIn[i] = 32;
+            else if (asciiIn[i] == 32 ) asciiIn[i]                     = 121;
+            else if (asciiIn[i] > 64 && asciiIn[i] < 91  )   asciiIn[i] = asciiIn[i] -= 1;
+            else if (asciiIn[i] > 96 && asciiIn[i] < 123  )  asciiIn[i] = asciiIn[i] -= 1;
+
+        }
+
+        in = convertToChar(asciiIn);
+        return new String(in);
+    }
+
+
+
+    private int[] reversNum(int[] asciiIn) {
+        int numbers = 0;
+        for (int i = 0; i < asciiIn.length; i++) {
+            System.out.println("i is "+i);
+            if (asciiIn[i] < 48 || asciiIn[i] > 57 && numbers > 0  && i <= asciiIn.length -1 ){
+                for (int j = i-numbers,k = i-1 ; j < k ; j++, k--) {
+                    System.out.println("k "+ k + " j "+j+" i "+i);
+                    int temp = asciiIn[j];
+                    asciiIn[j] = asciiIn[k];
+                    asciiIn[k] = temp;
+
+                }
+                numbers = 0;
+            }
+            else if (asciiIn[i] > 47 && asciiIn[i] < 58  ) {
+                numbers++;
+            }
+
+        }
+        char [] o = convertToChar(asciiIn);
+        String p = new String(o);
+        System.out.println(p);
+        return asciiIn;
+    }
+
+    private char[] convertToChar(int[] asciiIn) {
+        char[]toReturn = new char[asciiIn.length];
+        for(int i = 0; i < asciiIn.length;i++) {
+            toReturn[i] = (char) asciiIn[i];
+        }
+        return toReturn;
     }
 }
